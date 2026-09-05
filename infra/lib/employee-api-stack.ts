9c398ef83b518501e952d99fee25c64530adee2f
Abdul Hadi <116145502+Abdulhadi0007@users.noreply.github.com>
2026-09-05 13:39:48 +0530

    Allow ALB traffic to ECS dynamic ports

--- infra/lib/employee-api-stack.ts
+++ infra/lib/employee-api-stack.ts
@@ -7,7 +7,6 @@ import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as route53 from 'aws-cdk-lib/aws-route53';
-import * as targets from 'aws-cdk-lib/aws-route53-targets';
 
 export class EmployeeApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -18,6 +17,7 @@ export class EmployeeApiStack extends cdk.Stack {
       default: 't3.micro',
       description: 'EC2 type. t2.micro is also free-tier eligible in supported regions.'
     });
+
     const desiredCount = new cdk.CfnParameter(this, 'DesiredCount', {
       type: 'Number',
       default: 0,
@@ -25,16 +25,19 @@ export class EmployeeApiStack extends cdk.Stack {
       maxValue: 1,
       description: 'Number of running tasks. Capped at one for the free-tier setup.'
     });
+
     const imageTag = new cdk.CfnParameter(this, 'ImageTag', {
       type: 'String',
       default: 'latest',
       description: 'ECR image tag to run.'
     });
+
     const hostedZoneId = new cdk.CfnParameter(this, 'HostedZoneId', {
       type: 'String',
       default: '',
       description: 'Optional Route 53 hosted zone ID, without a trailing dot.'
     });
+
     const domainName = new cdk.CfnParameter(this, 'DomainName', {
       type: 'String',
       default: '',
@@ -44,11 +47,16 @@ export class EmployeeApiStack extends cdk.Stack {
     const vpc = new ec2.Vpc(this, 'Vpc', {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
       maxAzs: 2,
-      natGateways: 0,
+      natGateways: 1,
       subnetConfiguration: [
         {
           name: 'Public',
           subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24
+        },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
           cidrMask: 24
         }
       ]
@@ -86,12 +94,21 @@ export class EmployeeApiStack extends cdk.Stack {
       vpc,
       internetFacing: true
     });
+
     const loadBalancerSecurityGroup = loadBalancer.connections.securityGroups[0];
+
+    loadBalancerSecurityGroup.addEgressRule(
+      instanceSecurityGroup,
+      ec2.Port.tcpRange(32768, 65535),
+      'Allow health checks and application traffic to ECS dynamic host ports'
+    );
+
     instanceSecurityGroup.addIngressRule(
       loadBalancerSecurityGroup,
       ec2.Port.tcp(8080),
       'Allow the ALB to reach the application'
     );
+
     instanceSecurityGroup.addIngressRule(
       loadBalancerSecurityGroup,
       ec2.Port.tcpRange(32768, 65535),
@@ -100,32 +117,39 @@ export class EmployeeApiStack extends cdk.Stack {
 
     const machineImage = ecs.EcsOptimizedImage.amazonLinux2();
     const userData = ec2.UserData.forLinux();
+
     userData.addCommands(
       `echo ECS_CLUSTER=${cluster.clusterName} >> /etc/ecs/ecs.config`,
       'systemctl enable --now amazon-ssm-agent || true'
     );
+
     const launchTemplate = new ec2.LaunchTemplate(this, 'EcsLaunchTemplate', {
       machineImage,
       instanceType: new ec2.InstanceType(instanceType.valueAsString),
       role: instanceRole,
       securityGroup: instanceSecurityGroup,
       userData,
-      associatePublicIpAddress: true
+      associatePublicIpAddress: false
     });
+
     const autoScalingGroup = new autoscaling.AutoScalingGroup(this, 'EcsAutoScalingGroup', {
       vpc,
-      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+      },
       launchTemplate,
       minCapacity: 1,
       maxCapacity: 1,
       desiredCapacity: 1,
       healthCheck: autoscaling.HealthCheck.ec2()
     });
+
     const capacityProvider = new ecs.AsgCapacityProvider(this, 'AsgCapacityProvider', {
       autoScalingGroup,
       enableManagedScaling: false,
       enableManagedTerminationProtection: false
     });
+
     cluster.addAsgCapacityProvider(capacityProvider);
 
     const taskExecutionRole = new iam.Role(this, 'TaskExecutionRole', {
@@ -136,6 +160,7 @@ export class EmployeeApiStack extends cdk.Stack {
         )
       ]
     });
+
     repository.grantPull(taskExecutionRole);
 
     const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TaskDefinition', {
@@ -146,15 +171,19 @@ export class EmployeeApiStack extends cdk.Stack {
         assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com')
       })
     });
+
     const container = taskDefinition.addContainer('EmployeeApi', {
       image: ecs.ContainerImage.fromEcrRepository(repository, imageTag.valueAsString),
       memoryLimitMiB: 450,
       cpu: 256,
-      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'employee-api' }),
+      logging: ecs.LogDrivers.awsLogs({
+        streamPrefix: 'employee-api'
+      }),
       environment: {
         SERVER_PORT: '8080'
-      },
+      }
     });
+
     container.addPortMappings({
       containerPort: 8080,
       hostPort: 0,
@@ -166,7 +195,12 @@ export class EmployeeApiStack extends cdk.Stack {
       serviceName: 'employee-api-service',
       taskDefinition,
       desiredCount: desiredCount.valueAsNumber,
-      capacityProviderStrategies: [{ capacityProvider: capacityProvider.capacityProviderName, weight: 1 }],
+      capacityProviderStrategies: [
+        {
+          capacityProvider: capacityProvider.capacityProviderName,
+          weight: 1
+        }
+      ],
       healthCheckGracePeriod: cdk.Duration.seconds(90),
       minHealthyPercent: 0,
       maxHealthyPercent: 100,
@@ -177,6 +211,7 @@ export class EmployeeApiStack extends cdk.Stack {
       port: 80,
       open: true
     });
+
     listener.addTargets('EcsTarget', {
       port: 8080,
       protocol: elbv2.ApplicationProtocol.HTTP,
@@ -194,25 +229,30 @@ export class EmployeeApiStack extends cdk.Stack {
         cdk.Fn.conditionNot(cdk.Fn.conditionEquals(domainName.valueAsString, ''))
       )
     });
-    const zone = route53.HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+
+    const record = new route53.CfnRecordSet(this, 'ApiAliasRecord', {
       hostedZoneId: hostedZoneId.valueAsString,
-      zoneName: domainName.valueAsString
+      name: domainName.valueAsString,
+      type: 'A',
+      aliasTarget: {
+        dnsName: loadBalancer.loadBalancerDnsName,
+        hostedZoneId: loadBalancer.loadBalancerCanonicalHostedZoneId,
+        evaluateTargetHealth: false
+      }
     });
-    const record = new route53.ARecord(this, 'ApiAliasRecord', {
-      zone,
-      recordName: domainName.valueAsString,
-      target: route53.RecordTarget.fromAlias(new targets.LoadBalancerTarget(loadBalancer))
-    });
-    (record.node.defaultChild as route53.CfnRecordSet).cfnOptions.condition = dnsCondition;
+
+    record.cfnOptions.condition = dnsCondition;
 
     new cdk.CfnOutput(this, 'LoadBalancerUrl', {
       value: `http://${loadBalancer.loadBalancerDnsName}`,
       description: 'Public URL for the employee API.'
     });
+
     new cdk.CfnOutput(this, 'EcrRepositoryUri', {
       value: repository.repositoryUri,
       description: 'Push the employee-api image to this ECR repository.'
     });
+
     new cdk.CfnOutput(this, 'HealthCheckUrl', {
       value: `http://${loadBalancer.loadBalancerDnsName}/actuator/health`
     });
